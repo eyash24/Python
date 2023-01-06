2cf11abab791b6cf1c50e9d5c02a67dc3d1908b6
eyash24 <eyash.prasad24@gmail.com>
2023-01-06 22:09:09 +0530

    Second Commit

---
+++ Scientific Computing with Python/Program_7.py
@@ -0,0 +1,14 @@
+# lesson: Networking with Python + Networking: Write a Web Browser
+# Source: https://www.freecodecamp.org/learn/scientific-computing-with-python
+
+import socket
+my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+my_sock.connect(('data.pr4e.org',80))
+cmd='Get https://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+my_sock.send(cmd)
+while(True):
+    data = my_sock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode())
+my_sock.close()
